c87e3b57ec2f813bc733dcf044e7e087af010e04
yaohd <yaohd@inesadt.com>
2023-05-24 11:26:21 +0800

    feature:finish ch2 code

--- docs/ch2/2.16_add_meaningful_context.go
+++ docs/ch2/2.16_add_meaningful_context.go
@@ -28,8 +28,41 @@ func printGuessStatistics(candidate rune, count int) {
     fmt.Println(guessMessage)
 }
 
-type GuessStatusMessage struct {
+type GuessStatisticsMessage struct {
     number         string
     verb           string
     pluralModifier string
+}
+
+func (g *GuessStatisticsMessage) make(candidate rune, count int) string {
+    g.createPluralDependentMessageParts(count)
+    return fmt.Sprintf("There %s %s %c%s", g.verb, g.number, candidate, g.pluralModifier)
+}
+
+func (g *GuessStatisticsMessage) createPluralDependentMessageParts(count int) {
+    if count == 0 {
+        g.thereAreNoLetters()
+    } else if count == 1 {
+        g.thereIsOneLetter()
+    } else {
+        g.thereAreManyLetters(count)
+    }
+}
+
+func (g *GuessStatisticsMessage) thereAreManyLetters(count int) {
+    g.number = strconv.Itoa(count)
+    g.verb = "are"
+    g.pluralModifier = "s"
+}
+
+func (g *GuessStatisticsMessage) thereIsOneLetter() {
+    g.number = "1"
+    g.verb = "is"
+    g.pluralModifier = ""
+}
+
+func (g *GuessStatisticsMessage) thereAreNoLetters() {
+    g.number = "no"
+    g.verb = "are"
+    g.pluralModifier = "s"
 }
